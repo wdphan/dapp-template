fce2c081cd2cb8b76d7d57abddd449f9795ca571
William Phan <willdphan@gmail.com>
2022-06-22 14:28:27 -0400

    added modal css

--- src/components/BuyModal.jsx
+++ src/components/BuyModal.jsx
@@ -6,18 +6,18 @@ import Link from 'next/link'
 
 const BuyModal = ({ close, buyTokens }) => {
 	const styles = {
-		container: `h-full w-full flex flex-col `,
-		closeX: `w-full h-[50px] flex items-center justify-end mb-[20px] text-[#29283D]`,
-		title: `text-3xl font-bold flex flex-1 items-center mt-[20px] justify-center mb-[40px] text-[#29283D]`,
-		content: `flex w-full mb-[30px] text-xl justify-center text-[#29283D]`,
-		input: `w-[50%] h-[50px] bg-[#f7f6f2] rounded-lg p-[10px] flex mx-auto`,
-		inputBox: `w-full h-full flex items-center justify-center bg-[#f7f6f2] focus:outline-none`,
-		price: `w-full h-full flex justify-center items-center mt-[20px] font-bold text-3xl text-[#29283D]`,
-		buyBtn: `w-[20%] h-[50px] bg-[#000] mt-[40px] rounded-lg p-[10px] flex mx-auto text-white justify-center items-center cursor-pointer`,
-		loaderContainer: `w-full h-[500px] flex items-center justify-center`,
-		loader: `w-full h-full flex items-center justify-center`,
-		etherscan: `w-full h-full flex items-center justify-center text-green-500 text-2xl mt-[20px] font-bold cursor-pointer`,
-		success: `w-full h-full flex items-center justify-center text-xl mt-[20px] font-bolder`,
+		container: `h-[60vh] flex flex-col bg-[#1B1C2C] items-center justify-center relative`,
+		closeX: ` absolute top-2 left-[37rem] justify-end text-[#F28E54]`,
+		title: `text-4xl font-bold flex items-center justify-center text-white mb-5 mt-5`,
+		content: `flex w-full mb-[30px] text-xl justify-center text-[#A3A4A5] font-bold`,
+		input: `w-[50%] h-[70px] bg-[#1B1C2C] rounded-lg flex mx-auto`,
+		inputBox: `w-full h-full flex items-center justify-center bg-[#1B1C2C] `,
+		price: `w-full flex justify-center items-center font-bold text-3xl text-white mt-8 mb-4`,
+		buyBtn: `w-[20%] h-[50px] bg-[#29283D] rounded-xl p-[10px] flex mx-auto text-white justify-center items-center cursor-pointer font-bold`,
+		loaderContainer: ` h-[500px] flex items-center justify-center text-white`,
+		loader: `h-full flex items-center justify-center`,
+		etherscan: ` flex text-center items-center justify-center text-[#F28E54] text-2xl font-bold cursor-pointer mb-5`,
+		success: `w-[30rem] h-[5rem] flex items-center justify-center text-center text-xl  font-bolder mt-2`,
 	}
 	const {
 		amountDue,
@@ -61,7 +61,7 @@ const BuyModal = ({ close, buyTokens }) => {
 							className="cursor-pointer"
 						/>
 					</div>
-					<div className={styles.title}>Buy More Dropp Coins Here!</div>
+					<div className={styles.title}>Buy Dropp Coins</div>
 					<div className={styles.content}>Select how many tokens you would like to buy.</div>
 					<div className={styles.input}>
 						<input
@@ -88,7 +88,7 @@ const BuyModal = ({ close, buyTokens }) => {
 					{etherscanLink && (
 						<>
 							<div className={styles.success}>
-								Transaction Sucessful! Check out your receipt for your transaction below!
+								Transaction Sucessful. Check out your receipt for your transaction below!
 							</div>
 							<Link href={`${etherscanLink}`} className={styles.etherscan}>
 								<a className={styles.etherscan} target="_blank">
